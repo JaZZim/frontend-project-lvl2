a315b112bf30fa3398dc137e5ad4fbb54e5fd7a6
Anton Nikulin <hotjazzim@gmail.com>
2021-02-09 15:12:23 +0500

    connect commander

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
+import { Command } from 'commander';
 
-console.log('Hello, User');
+const program = new Command();
+
+program
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.');
+
+program.parse();
