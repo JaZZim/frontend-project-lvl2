ee1139062598e895c42c3e26ce254df57a708f6b
Anton Nikulin <hotjazzim@gmail.com>
2021-02-10 17:15:13 +0500

    add cases for testing

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,24 +1,25 @@
+import { describe } from '@jest/globals';
+import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
 
 // eslint-disable-next-line no-underscore-dangle
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const getFilePath = (filename) => path.join(__dirname, '../test_files', filename);
+const getFilePath = (filename) => path.join(__dirname, '../__fixtures__', filename);
 
-const expected = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`;
+const expectedData = { plain: [] };
 
-test('JSON. Primitive structure', () => {
-  const path1 = getFilePath('plain1.json');
-  const path2 = getFilePath('plain2.json');
-  const wrongPath = getFilePath('wrong_path.json');
-  expect(genDiff(path1, path2)).toBe(expected);
-  expect(() => genDiff(path1, wrongPath)).toThrow();
+beforeAll(() => {
+  const plainResult = fs.readFileSync(getFilePath('plain.result.txt'), 'utf-8');
+  expectedData.plain = plainResult.trim().split('\n\n\n');
+});
+
+describe.each([0, 1])('Primitive structure. Case - %#', (index) => {
+  test('JSON format', () => {
+    const path1 = getFilePath(`plain/case${index}.before.json`);
+    const path2 = getFilePath(`plain/case${index}.after.json`);
+    const expected = expectedData.plain[index];
+    expect(genDiff(path1, path2)).toBe(expected);
+  });
 });
