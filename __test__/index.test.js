691b12c886ca6693d595fb901173ca331238836f
Anton Nikulin <hotjazzim@gmail.com>
2021-02-11 10:36:48 +0500

    add test for nested structure

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -7,24 +7,27 @@ import genDiff from '../src/index.js';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const getFilePath = (filename) => path.join(__dirname, '../__fixtures__', filename);
 
-const expectedData = { plain: [] };
+let expectedData = [];
 
 beforeAll(() => {
-  const plainResult = fs.readFileSync(getFilePath('plain.result.txt'), 'utf-8');
-  expectedData.plain = plainResult.trim().split('\n\n\n');
+  const result = fs.readFileSync(getFilePath('result.txt'), 'utf-8');
+  expectedData = result.trim().split('\n\n\n');
 });
 
-describe.each([0, 1])('Primitive structure. Case - %#', (index) => {
+describe.each([
+  [0, 'Primitive'],
+  [1, 'Nested'],
+])('Case - %i, %s structure', (index) => {
   test('JSON format', () => {
-    const path1 = getFilePath(`plain/case${index}.before.json`);
-    const path2 = getFilePath(`plain/case${index}.after.json`);
-    const expected = expectedData.plain[index];
+    const path1 = getFilePath(`case${index}.before.json`);
+    const path2 = getFilePath(`case${index}.after.json`);
+    const expected = expectedData[index];
     expect(genDiff(path1, path2)).toBe(expected);
   });
   test('YAML format', () => {
-    const path1 = getFilePath(`plain/case${index}.before.yml`);
-    const path2 = getFilePath(`plain/case${index}.after.yml`);
-    const expected = expectedData.plain[index];
+    const path1 = getFilePath(`case${index}.before.yml`);
+    const path2 = getFilePath(`case${index}.after.yml`);
+    const expected = expectedData[index];
     expect(genDiff(path1, path2)).toBe(expected);
   });
 });
