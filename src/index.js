1e8fc0ddd64797def5d138430feba786e32bcdee
Anton Nikulin <hotjazzim@gmail.com>
2021-02-12 15:18:46 +0500

    add defalut formatter for lib

--- src/index.js
+++ src/index.js
@@ -1,30 +1,17 @@
-import sortBy from 'lodash/sortBy.js';
+import generateAst from './generateAst.js';
 import parseFile from './parsers.js';
+import stylish from './formatters/stylish.js';
 
-// function generateStringOfChange(key, value, level, change = 'unchanged') {
-//   const prefix = {
-//     add: '+ ',
-//     remove: '- ',
-//     unchanged: '  ',
-//   };
-//   return `${'  '.repeat(level)}${prefix[change]}${key}: ${value}`;
-// }
-
-export default function genDiff(pathFile1, pathFile2) {
+export default function genDiff(pathFile1, pathFile2, typeFormatter = 'stylish') {
+  const choiceFormatter = {
+    stylish,
+  };
   const obj1 = parseFile(pathFile1);
   const obj2 = parseFile(pathFile2);
-  const sortedAllKeys = sortBy(Object.keys({ ...obj1, ...obj2 }));
-  const result = sortedAllKeys.reduce((acc, key) => {
-    if (!Object.prototype.hasOwnProperty.call(obj1, key)) {
-      return [...acc, `  + ${key}: ${obj2[key]}`];
-    }
-    if (!Object.prototype.hasOwnProperty.call(obj2, key)) {
-      return [...acc, `  - ${key}: ${obj1[key]}`];
-    }
-    if (obj1[key] !== obj2[key]) {
-      return [...acc, `  - ${key}: ${obj1[key]}`, `  + ${key}: ${obj2[key]}`];
-    }
-    return [...acc, `    ${key}: ${obj1[key]}`];
-  }, []);
-  return ['{', ...result, '}'].join('\n');
+  const astTree = generateAst(obj1, obj2);
+  try {
+    return choiceFormatter[typeFormatter](astTree);
+  } catch (error) {
+    throw new Error('Unknown output format.');
+  }
 }
