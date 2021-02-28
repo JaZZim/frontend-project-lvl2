bf897942af84292d457ba4abc0931f725a2ad947
Anton Nikulin <hotjazzim@gmail.com>
2021-02-28 19:40:22 +0500

    refactor: change implementation of indentation

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,20 +2,15 @@ import _ from 'lodash';
 
 const singleIndent = 4;
 
-function getIndent(depth, type = 'unchanged') {
-  const countSpace = depth * singleIndent;
-  const prefix = {
-    added: '+ ',
-    removed: '- ',
-    unchanged: '',
-  };
-  return prefix[type].padStart(countSpace);
+function getIndent(depth, prefixSize) {
+  const countSpace = depth * singleIndent - prefixSize;
+  return ' '.repeat(countSpace);
 }
 
 const toString = (node, level) => {
   if (_.isPlainObject(node)) {
-    const currentIndent = getIndent(level + 1);
-    const bracketIndent = getIndent(level);
+    const currentIndent = getIndent(level + 1, 0);
+    const bracketIndent = getIndent(level, 0);
     const result = Object.entries(node).map(([key, value]) => (
       `${currentIndent}${key}: ${toString(value, level + 1)}`
     ));
@@ -29,19 +24,25 @@ const toString = (node, level) => {
 
 export default function formatToStylish(astTree) {
   const iter = (node, level) => {
-    const bracketIndent = getIndent(level - 1);
-    const lines = node.flatMap((item) => {
-      const { key, type, value } = item;
+    const bracketIndent = getIndent(level - 1, 0);
+    const lines = node.flatMap((line) => {
+      const { key, type, value } = line;
       switch (type) {
         case 'nested':
-          return `${getIndent(level)}${key}: ${iter(item.children, level + 1)}`;
+          return `${getIndent(level, 0)}${key}: ${iter(line.children, level + 1)}`;
         case 'changed':
           return [
-            `${getIndent(level, 'removed')}${key}: ${toString(value.valueBefore, level)}`,
-            `${getIndent(level, 'added')}${key}: ${toString(value.valueAfter, level)}`,
+            `${getIndent(level, 2)}- ${key}: ${toString(value.valueBefore, level)}`,
+            `${getIndent(level, 2)}+ ${key}: ${toString(value.valueAfter, level)}`,
           ];
+        case 'added':
+          return `${getIndent(level, 2)}+ ${key}: ${toString(value, level)}`;
+        case 'removed':
+          return `${getIndent(level, 2)}- ${key}: ${toString(value, level)}`;
+        case 'unchanged':
+          return `${getIndent(level, 0)}${key}: ${toString(value, level)}`;
         default:
-          return `${getIndent(level, type)}${key}: ${toString(value, level)}`;
+          throw new Error('In AST tree the type of change is incorrect');
       }
     });
     return [
